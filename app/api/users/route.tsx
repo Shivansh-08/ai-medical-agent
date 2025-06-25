b210560fa49fdb106c11bfb9d0d2560adaf83497
shivansh <shivanshsinghania827@gmail.com>
2025-06-26 01:47:23 +0530

    modifed route user

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -4,7 +4,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req:NextRequest,res:NextResponse){
+export async function POST(req:NextRequest){
         const user = await currentUser();
 
         try {
